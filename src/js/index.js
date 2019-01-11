6c8e76d37acd43c6c317459259b6504de3daa52c
Koen Michiels <koen.michiels@telenet.be>
2019-01-11 11:28:51 +0100

    index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
+const dropdown = document.querySelector('.Header-links--mobile-dropdown');
+const hamburger = document.querySelector('.Header-links--mobile-hamburger');
 
+function showdropdown() {
+  dropdown.classList.toggle('inactive');
+}
 
+hamburger.addEventListener('click', showdropdown);
