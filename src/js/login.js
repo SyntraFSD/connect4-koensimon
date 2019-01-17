dacb2d74e57164f2ecf6fc5f35e4ab399ae254a4
Jens van CoppernolleÃ <Jens.vancoppernolle@hotmail.com>
2019-01-17 16:06:55 +0100

    Ajax Login & Register

--- src/js/login.js
+++ src/js/login.js
@@ -22,18 +22,42 @@ function getFormData(form) {
   const inputFields = loginForm.querySelectorAll('input');
   const formData = {};
   inputFields.forEach(function (inputField) {
-    formData[inputField.username] = inputField.value;
+    formData[inputField.name] = inputField.value;
   });
   return formData;
+}
+
+function handleLoginRequest(event){
+  const request = event.target;
+  if(request.readyState === 4){
+    if(request.status >= 200 && request.status < 300){
+      console.log('success');
+      console.log(request);
+    } else {
+      console.log('error');
+      console.log(request);
+    }
+  }
+}
+
+function handleRegisterRequest(event){
+  const request = event.target;
+  if(request.readyState === 4){
+    if(request.status >= 200 && request.status < 300){
+      console.log('success');
+      console.log(request);
+    } else {
+      console.log('error');
+      console.log(request);
+    }
+  }
 }
 
 function login(event) {
   event.preventDefault();
   const formData = getFormData(loginForm);
   const request = new XMLHttpRequest();
-  request.addEventListener('readystatechange', function (event) {
-    console.log(event);
-  });
+  request.addEventListener('readystatechange', handleLoginRequest);
   request.open('POST', 'http://connect4.pienter.space/api/auth/login');
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify(formData));
@@ -42,8 +66,14 @@ function login(event) {
 function register(event){
   event.preventDefault();
   const formData = getFormData(registerForm);
+  const request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', handleRegisterRequest);
+  request.open('POST', 'http://connect4.pienter.space/api/auth/register');
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify(formData));
 }
 
 loginSwitch.addEventListener('click', showRegisterForm);
 registerSwitch.addEventListener('click', showLoginForm);
 loginForm.addEventListener('submit', login);
+registerForm.addEventListener('submit', register);
