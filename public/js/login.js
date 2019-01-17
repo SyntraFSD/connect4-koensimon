f18b3e903711a2998cd0871da01cdc1d50e73c74
simonpeeters1995 <simsimpeeeters@gmail.com>
2019-01-17 15:26:02 +0100

    Merge branch 'master' of https://github.com/SyntraFSD/connect4-koensimon
    # Conflicts:
    #	public/css/index.css.map

---
+++ public/js/login.js
@@ -0,0 +1,44 @@
+var loginSwitch = document.querySelector('.login-switch');
+var registerSwitch = document.querySelector('.register-switch');
+var loginForm = document.querySelector('.wrapper');
+var registerForm = document.querySelector('.wrapper-registration');
+
+function switchForm(fromForm, toForm) {
+  fromForm.classList.add('hide');
+  toForm.classList.remove('hide');
+}
+
+function showLoginForm(event) {
+  event.preventDefault();
+  switchForm(registerForm, loginForm);
+}
+
+function showRegisterForm(event) {
+  event.preventDefault();
+  switchForm(loginForm, registerForm);
+}
+
+function getFormData(form) {
+  var inputFields = loginForm.querySelectorAll('input');
+  var formData = {};
+  inputFields.forEach(function (inputField) {
+    postData[inputField.username] = inputField.value;
+  });
+  return formData;
+}
+
+function login(event) {
+  event.preventDefault();
+  var formData = getFormData(loginForm);
+  var request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', function (event) {});
+  request.open('POST', '');
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify(formData));
+  loginSwitch.addEventListener('click', showRegisterForm);
+  registerSwitch.addEventListener('click', showLoginForm);
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+  });
+}
+//# sourceMappingURL=login.js.map
